a0979efd7ab9150792d7d41853a0b597d3051abf
ThariniPrabakaran <110752950+ThariniPrabakaran@users.noreply.github.com>
2022-08-07 08:52:06 +0530

    Add files via upload
    Week1

---
+++ week1/day1/MyCar.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class MyCar {
+
+	 byte numberOfCar = 1 ;
+	public short totalSeats = 5;
+	private int yearOfManufacture = 2021 ;
+	float totalPrice = 250000.67474F ;
+	long serialNumber = 9342433432422422L;
+	char series ='X';
+	String modelName = "Nissan Rouge";
+	boolean isCarNew = true ;
+	double 	insuranceNumber =343423432 ;
+	
+}
---
+++ week1/day1/TwoWheeler.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int noOfWheels = 2;
+		short noOfMirrors = 2;
+		long chassisNumber = 12345678912242022l;
+		boolean isPunctured = false;
+		String bikeName = "Activa";
+		double runningKM = 2500.345;
+		
+		System.out.println(bikeName);
+		System.out.println(noOfWheels);
+		System.out.println(noOfMirrors);
+		System.out.println(chassisNumber);
+		System.out.println(isPunctured);
+		System.out.println(bikeName);
+		System.out.println(runningKM);
+		
+		
+		
+	}
+
+}
