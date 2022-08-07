b5f8f8b2e34ab47823cfaf4c013e71a3322b38e0
ThariniPrabakaran <110752950+ThariniPrabakaran@users.noreply.github.com>
2022-08-07 09:00:31 +0530

    Add files via upload
    week1

---
+++ week1/day1/CheckIfNumberIsPositiveOrNegative.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class CheckIfNumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 0 ;
+		
+		if (a>0)
+		{
+			System.out.println("a is positive number");
+		}
+		if (a<0) {
+			
+			System.out.println("a is negative number");
+		}
+		
+		if (a==0) {
+			System.out.println("a is neither negative nor positive");
+		}
+
+	}
+
+}
---
+++ week1/day1/ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class ConvertANegativeNumberToPositiveNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int b = -5;
+		
+		if (b<0) {
+			
+			int c = b *-1;
+			System.out.println(c);
+		}
+	
+	}
+
+}
